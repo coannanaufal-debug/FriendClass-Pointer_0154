515ec09125279a593cbd5615f788456d06bc7678
coannanaufal-debug <coanna.naufal@gmail.com>
2026-04-27 15:45:46 +0700

    membuat objek budi dan display untuk menampilkan

--- friendfunctionDua.cpp
+++ friendfunctionDua.cpp
@@ -7,3 +7,25 @@ class manusia
     public:
     void showallpelajar(pelajar &x);
 };
+
+class pelajar
+{
+    private:
+    int nilai;
+    public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showallpelajar(pelajar &x);
+};
+
+void manusia::showallpelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showallpelajar(pbudi);
+    return 0;
+}
