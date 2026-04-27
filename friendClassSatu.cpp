7faa906eb15907532c145b4389d8d45b7d6cfb20
coannanaufal-debug <coanna.naufal@gmail.com>
2026-04-27 15:18:44 +0700

    membuat class siswa dan menjadikan id private

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -8,7 +8,20 @@ class orang
 private:
     string nama;
 public:
-    void setNama(string pNama){
+    void setNama(string pNama)
         friend class siswa; // deklarasi friend class
+    
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a)
+    {
+        cout << id << endl << a.nama << endl;
     }
 }
