572982ba1c8a6eb64a5aae211988f0cc6be5d3c1
coannanaufal-debug <coanna.naufal@gmail.com>
2026-04-27 15:26:09 +0700

    membuat objek joko

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -36,3 +36,13 @@ void siswa::setId(int pId)
 {
     id = pId;
 }  
+
+int main()
+{
+    orang joko;
+    joko.setNama("Joko kumat");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
